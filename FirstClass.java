56ca3bf36a8856f4772804c9c78af8027fcf4bd9
lmargain <787105@seq.org>
2016-08-24 08:49:27 -0700

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,25 @@
+
+public class FirstClass
+{
+    // instance variables - replace the example below with your own
+    private int a;
+    private int b;
+
+    public FirstClass()
+    {
+        // initialise instance variables
+        a = 2;
+        b = 5;
+    }
+
+    
+    public int addEmUp()
+    {
+        return a + b;
+    }
+
+    public int difference () {
+        return a - b;
+    }
+
+}
