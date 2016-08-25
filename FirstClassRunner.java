15eab7ad4ef51709c279c9bd1dca0f48e7f297fa
lmargain <787105@seq.org>
2016-08-24 20:19:51 -0700

    Add files via upload

--- FirstClassRunner.java
+++ FirstClassRunner.java
@@ -8,26 +8,13 @@
 public class FirstClassRunner
 {
     // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class FirstClassRunner
-     */
-    public FirstClassRunner()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static void main(String[] args){
+        FirstClass fc = new FirstClass();
+        
+        
+        System.out.print(fc.addEmUp());
+        System.out.print(",");
+        System.out.println(fc.difference());
+        
     }
 }
